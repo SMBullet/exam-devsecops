9875fbe9568aded9fcd22a0ed38373a57462ee45
Mehdi Stoti <mehdistoti01@gmail.com>
2024-10-28 09:43:56 +0100

    initialisation du projet

---
+++ src/main/java/com/exam/gestionemploye/GestionemployeApplication.java
@@ -0,0 +1,13 @@
+package com.exam.gestionemploye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionemployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionemployeApplication.class, args);
+	}
+
+}
